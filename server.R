9343adbbb4d9e09293f481017e51d824fc1d3411
Chun Tao <taochun01@gmail.com>
2020-10-11 21:56:36 -0400

    add infobox

--- server.R
+++ server.R
@@ -1,24 +1,17 @@
 
 shinyServer(function(input, output){
   
-  output$maxBox <- renderInfoBox({
-    max_value <- max(state_stat[,input$selected])
-    max_state <- 
-      state_stat$state.name[state_stat[,input$selected]==max_value]
-    infoBox(max_state, max_value, icon = icon("hand-o-up"))
+  output$num_store <- renderInfoBox({
+    num_store <- nrow(world)
+    infoBox("Number of stores", num_store)
   })
   
-  output$minBox <- renderInfoBox({
-    min_value <- min(state_stat[,input$selected])
-    min_state <- 
-      state_stat$state.name[state_stat[,input$selected]==min_value]
-    infoBox(min_state, min_value, icon = icon("hand-o-down"))
+  output$num_country <- renderInfoBox({
+    num_country <- length(unique(world$country))
+    infoBox("Number of countries", num_country)
   })
   
-  output$avgBox <- renderInfoBox(
-    infoBox(paste("AVG.", input$selected),
-            mean(state_stat[,input$selected]), 
-            icon = icon("calculator"), fill = TRUE))
+  
   
   output$hist = renderGvis(
     gvisHistogram(state_stat[,input$selected,drop=F])
@@ -26,7 +19,7 @@ shinyServer(function(input, output){
   )
   
   output$map = renderGvis({
-    gvisGeoChart(state_stat, "state.name", input$selected,
+    gvisGeoChart(world, "state.name", input$selected,
                  options=list(region="US", displayMode="regions", 
                               resolution="provinces",
                               width="auto", height="auto"))
@@ -34,9 +27,7 @@ shinyServer(function(input, output){
   
   
   output$table <- DT::renderDataTable({
-    datatable(state_stat, rownames=FALSE) %>% 
-      formatStyle(input$selected,  
-                  background="skyblue", fontWeight='bold')
+    datatable(world, rownames=FALSE) 
     
   })
   
