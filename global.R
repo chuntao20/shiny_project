ffe114e85b1913b3be0572a2c28676553c17d576
Chun Tao <taochun01@gmail.com>
2020-10-11 20:52:38 -0400

    add dataset and input selections in global file

--- global.R
+++ global.R
@@ -4,3 +4,13 @@ library(googleVis)
 library(DT)
 library(dplyr)
 
+
+world = read.csv('world.csv',header=T)
+
+
+country = unique(world$country)
+ownership = unique(world$ownership_type)
+brand = unique(world$brand)
+continent = unique(world$continent)
+
+
