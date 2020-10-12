2e8575dcb951e251607bfe06cb0e0d4eecf37127
Chun Tao <taochun01@gmail.com>
2020-10-11 20:33:43 -0400

    ui layout draft

--- ui.R
+++ ui.R
@@ -1,15 +1,28 @@
 
 
 dashboardPage(
-  dashboardHeader(title = 'My Dashboard'),
+  
+  
+  dashboardHeader(title = 'Starbucks Global Location Analysis'),
+  
+  
   dashboardSidebar(
-    sidebarUserPanel('Chun',
-                     image = 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRxYFZ9ljFFrqXwKlOEF24jwEQPeo41lgppeQ&usqp=CAU'),
+    
+    sidebarUserPanel("Author: Chun Tao"),
     
     sidebarMenu(
-      menuItem('Map',tabName = 'map', icon = icon('map')),
-      
-      menuItem('Data',tabName = 'data',icon = icon('database'))
+          menuItem('Overview',
+                   tabName = 'intro', icon = icon('thumbtack')),
+          
+          menuItem('Global Presents',
+                   tabName = 'summary', icon = icon('globe')),
+          
+          menuItem('Analysis',
+                   tabName = 'analysis', icon = icon('chart-bar')),
+          
+          menuItem('Data',
+                   tabName = 'data',icon = icon('database'))
+          
     ),
     
     selectizeInput('selected','Select Item to Display',
@@ -17,21 +30,80 @@ dashboardPage(
     
   ),
   
+  
   dashboardBody(
+    
+    ###font
+    
     tabItems(
-      tabItem(tabName = 'map',
+      
+      tabItem(tabName = 'intro', h2("Project Introduction"),
               
-              fluidRow(infoBoxOutput("maxBox"),
-                       infoBoxOutput("minBox"),
-                       infoBoxOutput("avgBox")),
+              fluidRow(),
+              
+              fluidRow()
+      ),
+      
+      tabItem(tabName = 'summary', h2("Glocal Presents of Stabucks Stores"),
+              
+              fluidRow(infoBoxOutput("num_store"),
+                       infoBoxOutput("num_country"),
+                       ),
               
               fluidRow(
-                box(htmlOutput('hist'),
-                    height=350),
                 box(htmlOutput('map'),
+                    height=350),
+                box(htmlOutput('store_by_top10'),
+                    height=350)
+              ),
+              
+              fluidRow(
+                box(htmlOutput('store_by_continent'),
+                    height=350),
+                box(htmlOutput('avg_store_by_continent'),
                     height=350)
               )
       ),
+      
+      
+      tabItem(tabName = 'analysis', h2("Expansion Strategy Analysis"),
+              
+              
+              fluidRow(
+                box(htmlOutput('by_brand'), #select full data/usa/top10
+                    height=350),
+                box(htmlOutput('brand_country'), #select individual country
+                    height=350)
+              ),
+              
+              fluidRow(
+                box(htmlOutput('by_ownership'), #select full data/usa/top10
+                    height=350),
+                box(htmlOutput('avg_store_by_continent'), #select individual country
+                    height=350)
+              )
+      ),
+      
+      
+      tabItem(tabName = 'analysis2', h2("Expansion Strategy Analysis"),
+              
+              
+              fluidRow(
+                box(htmlOutput('by_brand'), #select full data/usa/top10
+                    height=350),
+                box(htmlOutput('brand_country'), #select individual country
+                    height=350)
+              ),
+              
+              fluidRow(
+                box(htmlOutput('by_ownership'), #select full data/usa/top10
+                    height=350),
+                box(htmlOutput('avg_store_by_continent'), #select individual country
+                    height=350)
+              )
+      ),
+      
+      
       tabItem(tabName = 'data' ,
               fluidRow(
                 box(DT::dataTableOutput('table'),
