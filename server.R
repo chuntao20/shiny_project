5f2e5fce3bd7cbe645aad856ee6292ba92120de4
Chun Tao <taochun01@gmail.com>
2020-10-11 22:02:41 -0400

    add output plots

--- server.R
+++ server.R
@@ -1,6 +1,9 @@
 
 shinyServer(function(input, output){
   
+  
+#-------------summary------------------
+  
   output$num_store <- renderInfoBox({
     num_store <- nrow(world)
     infoBox("Number of stores", num_store)
@@ -11,19 +14,57 @@ shinyServer(function(input, output){
     infoBox("Number of countries", num_country)
   })
   
-  
-  
-  output$hist = renderGvis(
-    gvisHistogram(state_stat[,input$selected,drop=F])
-    
-  )
-  
   output$map = renderGvis({
     gvisGeoChart(world, "state.name", input$selected,
                  options=list(region="US", displayMode="regions", 
                               resolution="provinces",
                               width="auto", height="auto"))
   })
+  
+  
+  output$store_by_top10 = renderGvis(
+    gvisHistogram(state_stat[,input$selected,drop=F])
+    
+  )
+  
+  
+  
+  output$store_by_continent = renderGvis(
+    
+  )
+  
+  
+  output$avg_store_by_continent = renderGvis(
+    
+  )
+  
+  
+  #-------------analysis------------------
+  
+  
+  output$by_brand = renderGvis(
+    
+  )
+  
+  
+  output$brand_country = renderGvis(
+    
+  )
+  
+  
+  output$by_ownership = renderGvis(
+    
+  )
+  
+  
+  output$avg_store_by_continent = renderGvis(
+    
+  )
+  
+  
+  
+  #-------------table------------------
+  
   
   
   output$table <- DT::renderDataTable({
