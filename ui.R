cfaf69d524dbbdf408e06d5a7de70c50407113a5
Chun Tao <taochun01@gmail.com>
2020-10-18 16:29:19 -0400

    first tab content updated

--- ui.R
+++ ui.R
@@ -40,38 +40,32 @@ dashboardPage(
       tabItem(
               tabName = 'intro', 
               
-              h2("Project Summary"),
+              img(src="stb.jpg", width='480'),
               
-              h3("Starbucks is one of the biggest coffee brands in the world. Since founded in 1971 in Seattle, Washington, it has been constantly growing and now has more than 22,519 stores worldwide (starbuscks.com). The purpose of this project is to understand the underlying logic of Starbucks global expansion strategy. The primary data set was collected in Feb 2017, containing 26,000+ Startbucks store location in 73 countries."),
-      
-              h3('The Shiny dashboard includes 4 tads. The overview tab gives a breif introduction of the project. The global presents tab shows the geographic location for all Starbucks stores. The Analysis tabs shows the store distribution by brands and ownership types. The data tab is the original dataset used in the project.'),
+              h2("Project Summary"),
+              br(),
+              h3("Starbucks is one of the biggest coffee brands in the world. Since founded in 1971 in Seattle, Washington, it has been constantly growing and now has more than 22,519 stores worldwide (starbuscks.com). The purpose of this project is to understand the underlying logic of Starbucks store openning strategy, so as to extract transforable insights for other similar brands. The primary data set was collected in Feb 2017, containing 25,600 Startbucks store location information in 73 countries and areas."),
+              h3('The Shiny dashboard includes 4 tabs. The project summary tab gives a breif introduction of the project and the data resource. The Starbucks world tab shows the geographic location  and store counts information for all Starbucks stores. The two Analysis tabs give a more indepth look into the factors decide whether or not to open a store in a country, as well as how to open the store. The data tab is the primary dataset used in the project.'),
       
               br(),
               br(),
               
               h3("About Author"),
+              h4('Chun Tao has a master degree in Education Psychology from University of Pennsylvania.'),
               
-              h4('Chun has a master degree in Education Psychology from University of Pennsylvania.'),
-              
-              h3("Github"),
-              
-              h4('https://github.com/chuntao20/shiny_project'),
+              h3("Find the Author:"),
+              h4('Gihub:https://github.com/chuntao20/shiny_project'),
+              h4('Email: taochun01@gmail.com'),
               
               br(),
               br(),
               
               h3('Data Resources:'),
-              
               h4('Starbucks Location: https://www.kaggle.com/starbucks/store-locations'),
-              
-              h4('country code: https://datahub.io/core/country-list#data'),
-              
+              h4('Country code: https://datahub.io/core/country-list#data'),
               h4('GDP: https://data.worldbank.org/indicator/NY.GDP.MKTP.CD?end=2016&start=2015'),
-              
-              h4('Corruption: https://data.world/bchamptx/2015-corruption-v-gdp'),
-              
-              
-              h4('Population: https://population.un.org/wpp/Download/Standard/CSV')
+              h4('Corruption & Easy-to-Do-Business-Index: https://data.world/bchamptx/2015-corruption-v-gdp'),
+              h4('Country Population: https://population.un.org/wpp/Download/Standard/CSV')
               
               ),
       
